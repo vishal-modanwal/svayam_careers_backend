c5a19bf93bc86f2908ef6e38c0909a79b08e6a4d
vishal modanwal <vk076394@gmail.com>
2026-06-03 10:14:11 +0530

    last fix

--- src/app.js
+++ src/app.js
@@ -26,7 +26,10 @@ app.use(cors({
 }));
 app.use(express.json());
 
-
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path}`);
+  next();
+});
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Routes
